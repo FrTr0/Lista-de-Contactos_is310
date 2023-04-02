043b72a6ca463e9a5301ec2e8e0e7852f90df4db
FrTr0 <129633646+FrTr0@users.noreply.github.com>
2023-04-01 19:24:59 -0600

    Update Lista_Contactos.java
    cambie el texto que se imprime y agregue un contacto mas para demostrar el funcionamiento del compareTo

--- src/main/java/Lista_Contactos.java
+++ src/main/java/Lista_Contactos.java
@@ -1,31 +1,29 @@
 import java.util.Collections;
 import java.util.LinkedList;
 
-
 public class Lista_Contactos{
     public static void main(String[] args) {
         LinkedList<Persona> contactos = new LinkedList<>();
         Persona p1 = new Persona("Emilio","Molina","8744-5450","Barrio Buenos Aires","Emiliomolina266@gmail.com");
         Persona p2 = new Persona("Cristian","Molina","8744-5450","Barrio Buenos Aires","Emiliomolina266@gmail.com");
-        Persona p3 = new Persona("Oscar","ALMENDAREZ","8744-5450","Barrio Buenos Aires","Emiliomolina266@gmail.com");
-        Persona p4 = new Persona("Angie","NUNEZ","8744-5450","Barrio Buenos Aires","Emiliomolina266@gmail.com");
+        Persona p3 = new Persona("Oscar","Almendarez","8744-5450","Barrio Buenos Aires","Emiliomolina266@gmail.com");
+        Persona p4 = new Persona("Angie","Nuñez","8744-5450","Barrio Buenos Aires","Emiliomolina266@gmail.com");
+        Persona p5 = new Persona("Angie","Morales","8744-5450","Barrio Buenos Aires","Emiliomolina266@gmail.com");
+        
         contactos.add(p1);
         contactos.add(p2);
         contactos.add(p3);
         contactos.add(p4);
+        contactos.add(p5);
 
 
-
-        System.out.println(contactos.toString());
         Ordenar(contactos);
-        System.out.println(contactos.toString());
+        System.out.println("\n" + contactos.toString().replaceAll("[\\[\\],]\\s*", ""));
 
     }
 
    public static void Ordenar(LinkedList<Persona> lista){
        Collections.sort(lista);
-
    }
-
-
+   
 }
